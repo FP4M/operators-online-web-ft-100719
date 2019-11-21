d6063d5fa9efc101178ac937e500a1e6806eb323
Prince Charles <princecharles35@yahoo.com>
2019-11-21 01:21:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
-speed = 40
-if speed < 40 
-  "unsafe"
-  if speed > 60
-    "unsafe"
-  else 
-    "safe" 
-    
-    speed = 40
-    speed < 40 ? "unsafe" speed > 60 ? 'unsafe' : 'safe'
-  
+def unsafe(speed)
+	if speed > 60 	
+		true	
+	elsif 	
+		speed < 40	
+		true	
+	else	
+		false	
+	end	
+end	
+
+def not_safe(speed)	
+	speed < 40 || speed > 60 ? true : false	
+end
